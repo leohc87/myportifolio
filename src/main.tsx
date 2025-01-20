d4ce421062d3c0bc644c38a565e2c73eb30a8704
leohc87 <38429138+leohc87@users.noreply.github.com>
2025-01-20 15:55:13 -0300

    feat: configs material ui theme and css baseline

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import React from 'react'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import theme from './theme.ts'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
     <App />
-  </StrictMode>,
+    </ThemeProvider>
+  </React.StrictMode>,
 )
---
+++ src/theme.ts
@@ -0,0 +1,19 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme ({
+    palette: {
+        primary: {
+            main: '#232323',
+        },
+        secondary: {
+            main: '#4f8e3e',
+        },
+    },
+    typography: {
+        fontFamily: [
+            '"Helvetica Neue"',
+        ].join(','),
+    }
+});
+
+export default theme;
